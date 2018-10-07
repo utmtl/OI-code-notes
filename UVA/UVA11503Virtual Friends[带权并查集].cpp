76e982d62037f7a4a314f48137a2ca7ef63e1c72
xcc <35071113+xcc23@users.noreply.github.com>
2018-10-07 08:27:55 +0800

    Update UVA11503Virtual Friends[带权并查集].cpp

--- UVA/UVA11503Virtual Friends[带权并查集].cpp
+++ UVA/UVA11503Virtual Friends[带权并查集].cpp
@@ -1,11 +1,9 @@
-//带权并查集 
 #include<iostream>
 #include<cstdio>
 #include<map>
 using namespace std;
-int f[210000],sum[210000]; //注意：取名rank有玄学错误！ 
+long long f[210000],sum[210000]; //注意：取名rank有玄学错误！ 
 map<string,int> mmp; //表达对出题人崇高的敬意 （不是指我的教练） 
-//注意把名字映射成编号 
 int m;
 int find(int x)
 {
@@ -15,29 +13,30 @@ int find(int x)
 inline int u(int r1,int r2)
 {
 	f[r2]=r1;
-	sum[r1]+=sum[r2]; //在合并时把儿子的节点数加入父亲的节点数即可完成元素数统计 
+	sum[r1]+=sum[r2];
 	return sum[r1];
 }
 int main()
 {
 	string r1,r2;
 	int t;
-	scanf("%d",&t);
-	while(t-->0){
-		int cnt=0;
-		scanf("%d",&m);
-		for(int i=1;i<210000;i++) f[i]=i,sum[i]=1; //m个朋友关系最多有m*2个人！不是m个人！ 
-		for(int i=1;i<=m;i++)
-		{
-			cin>>r1>>r2;
-			if(mmp.count(r1)==0) mmp[r1]=++cnt;
-			if(mmp.count(r2)==0) mmp[r2]=++cnt;
-			int x1=find(mmp[r1]);
-			int x2=find(mmp[r2]);
-			if(x1!=x2) printf("%d\n",u(x1,x2));
-			else printf("%d\n",sum[x1]);
+	while(scanf("%d",&t)!=EOF){ //巨坑，第一行的数字t不是总的测试数据组数，可能有不确定的多组数据，每组都有一个t，每个t下面又有t个n
+		while(t-->0){
+			int cnt=0;
+			scanf("%d",&m);
+			for(int i=0;i<=2*m+1;i++) f[i]=i,sum[i]=1; //m个朋友关系最多有m*2个人！不是m个人！ 
+			for(int i=1;i<=m;i++)
+			{
+				cin>>r1>>r2;
+				if(mmp.count(r1)==0) mmp[r1]=++cnt;
+				if(mmp.count(r2)==0) mmp[r2]=++cnt;
+				int x1=find(mmp[r1]);
+				int x2=find(mmp[r2]);
+				if(x1!=x2) printf("%d\n",u(x1,x2));
+				else printf("%d\n",sum[x1]);
+			}
+			mmp.clear();
 		}
-		mmp.clear();
 	}
 	return 0;
 }
