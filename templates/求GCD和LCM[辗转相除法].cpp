e81e9e1d98ed3ff2ef6b5a852b7dd344460db292
xcc <35071113+xcc23@users.noreply.github.com>
2018-12-01 10:51:40 +0800

    Update 求GCD和LCM[辗转相除法].cpp

--- templates/求GCD和LCM[辗转相除法].cpp
+++ templates/求GCD和LCM[辗转相除法].cpp
@@ -7,9 +7,9 @@ int gcd_DestoryStack(int a,int b)
 int gcd(int a,int b)
 {
     while(b^=a^=b^=a%=b);
-	return a;
-    /*line 4解析:
-	(注意b^=...等缩写的运算返回运算后的b值)
+    return a;
+/*line 4解析:
+    (注意b^=...等缩写的运算返回运算后的b值)
     a%=b;
     b^=a^=b^=a;
     即
@@ -21,13 +21,13 @@ int gcd(int a,int b)
     a%=b;
     swap(a,b);
     执行条件是b!=0
-	即
-	while(b!=0)
-	{
-	    a%=b; //运算后a<b
-		swap(a,b); //要保证a中的值始终比b大
-	}
-    */
+    即
+    while(b!=0)
+    {
+    	a%=b; //运算后a<b
+	swap(a,b); //要保证a中的值始终比b大
+    }
+*/
 }
 int main()
 {
